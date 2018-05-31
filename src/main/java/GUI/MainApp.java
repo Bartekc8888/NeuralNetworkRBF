8457c6078a14965e91747493bd1cf00c3fe48097
Bartlomiej Ciesielski <bartekc8@op.pl>
2018-05-31 14:40:25 +0200

    Podlaczenie interfejsu uzytkownika

--- src/main/java/GUI/MainApp.java
+++ src/main/java/GUI/MainApp.java
@@ -30,16 +30,6 @@ public class MainApp extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        
-        /*
-        NetworkConfiguration configuration = getConfigurationOfTask1();
-        
-        NetworkManager manager = new NetworkManager(configuration, new ErrorPlotter(),
-                new AproximationEndResultProcessor());
-        
-        Thread algorithmThread = new Thread(manager);
-        algorithmThread.start();
-        */
     }
     
     public static void startLearning(SettingsFromUserTransportObject settings) {
@@ -72,97 +62,6 @@ public class MainApp extends Application {
         }
     }
 
-    public static NeuralLayerProperties[] getPropertiesOfTask1() {
-        NeuralLayerProperties[] properties = new NeuralLayerProperties[2];
-        
-        NeuralLayerPropertiesBuilder builder = new NeuralLayerPropertiesBuilder();
-        builder.setNeuronCount(10)
-        .setRadialActivationFunction(new GaussianFunction())
-        .setBackpropagationUsed(false)
-        .setCenterSelectorType(CenterSelectorType.Random)
-        .setInertialInfluence(0.5)
-        .setInputCount(1)
-        .setLayerType(LayerType.Gaussian)
-        .setLearningRate(0.6);
-        
-        properties[0] = builder.build();
-        
-        builder.setActivationFunction(new LinearFunction())
-        .setLayerType(LayerType.Linear)
-        .setNeuronCount(1)
-        .setBiasUsed(true)
-        .setInputCount(10)
-        .setBackpropagationUsed(true);
-        
-        properties[1] = builder.build();
-        
-        return properties;
-    }
-    
-    public static NetworkConfiguration getConfigurationOfTask1() {
-        SettingsFromUserTransportObject settings = new SettingsFromUserTransportObject();
-        settings.type = SettingsType.Type3a;
-        settings.epochLimit = 5000;
-        settings.errorLimit = 0.05;
-        settings.neuronCount = 10;
-        settings.inertialInfluence = 0.3;
-        settings.learningRate = 0.7;
-        settings.trainingFile = "data/approximation_train_1.txt";
-        settings.testingFile = "data/approximation_test.txt";
-        
-        NetworkConfiguration configuration = NetworkConfigurationFactory.createNetworkConfiguration(settings);
-        
-        return configuration;
-    }
-    
-    public static NetworkConfiguration getConfigurationOfTask2() {
-        SettingsFromUserTransportObject settings = new SettingsFromUserTransportObject();
-        settings.type = SettingsType.Type2;
-        settings.epochLimit = 5000;
-        settings.errorLimit = 0.001;
-        settings.neuronCount = 10;
-        settings.inertialInfluence = 0.3;
-        settings.learningRate = 0.7;
-        settings.trainingFile = "data/classification_train.txt";
-        settings.testingFile = "data/classification_test.txt";
-        
-        NetworkConfiguration configuration = NetworkConfigurationFactory.createNetworkConfiguration(settings);
-        
-        return configuration;
-    }
-    
-    public static NetworkConfiguration getConfigurationOfTask3a() {
-        SettingsFromUserTransportObject settings = new SettingsFromUserTransportObject();
-        settings.type = SettingsType.Type3a;
-        settings.epochLimit = 5000;
-        settings.errorLimit = 0.05;
-        settings.neuronCount = 10;
-        settings.inertialInfluence = 0.3;
-        settings.learningRate = 0.7;
-        settings.trainingFile = "data/approximation_train_1.txt";
-        settings.testingFile = "data/approximation_test.txt";
-        
-        NetworkConfiguration configuration = NetworkConfigurationFactory.createNetworkConfiguration(settings);
-        
-        return configuration;
-    }
-    
-    public static NetworkConfiguration getConfigurationOfTask3b() {
-        SettingsFromUserTransportObject settings = new SettingsFromUserTransportObject();
-        settings.type = SettingsType.Type3b;
-        settings.epochLimit = 5000;
-        settings.errorLimit = 0.001;
-        settings.neuronCount = 10;
-        settings.inertialInfluence = 0.3;
-        settings.learningRate = 0.7;
-        settings.trainingFile = "data/classification_train.txt";
-        settings.testingFile = "data/classification_test.txt";
-        
-        NetworkConfiguration configuration = NetworkConfigurationFactory.createNetworkConfiguration(settings);
-        
-        return configuration;
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         URL url = getClass().getClassLoader().getResource(mainWindowFXML);
@@ -171,7 +70,6 @@ public class MainApp extends Application {
         Scene scene = new Scene(root);
         
         mainWindowController = loader.getController();
-        //initMainWindowEvents(mainWindowController);
         
         this.primaryStage = primaryStage;
         primaryStage.setTitle("Sieci RBF");
--- src/main/java/GUI/MainWindowController.java
+++ src/main/java/GUI/MainWindowController.java
@@ -1,15 +1,26 @@
 package GUI;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import NetworkUtilities.Configuration.SettingsFromUserTransportObject;
 import NetworkUtilities.Configuration.SettingsType;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class MainWindowController  implements Initializable {
 
@@ -22,7 +33,7 @@ public class MainWindowController  implements Initializable {
     @FXML
     private Spinner<Double> inertiaSpinner;
     @FXML
-    private Spinner<Integer> errorLimitSpinner;
+    private Spinner<Double> errorLimitSpinner;
     @FXML
     private Spinner<Integer> epochLimitSpinner;
     @FXML
@@ -37,10 +48,89 @@ public class MainWindowController  implements Initializable {
     @FXML
     private Button startLearningButton;
     
-    
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        // TODO Auto-generated method stub
+        setupCombobox();
+        setupSpinners();
+        setupButtons();
+    }
+    
+    private void setupCombobox() {
+        SettingsType[] values = SettingsType.values();
+        ObservableList<SettingsType> settingTypes = FXCollections.observableArrayList(values);
         
+        settingsTypeCombobox.setItems(settingTypes);
+        settingsTypeCombobox.getSelectionModel().select(0);
+    }
+    
+    private void setupSpinners() {
+        neuronCountSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 30, 10, 1));
+        neuronCountSpinner.setEditable(true);
+        
+        learningRateSpinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0.001, 2, 0.6, 0.1));
+        learningRateSpinner.setEditable(true);
+        
+        inertiaSpinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 2, 0.4, 0.1));
+        inertiaSpinner.setEditable(true);
+        
+        errorLimitSpinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 1, 0.05, 0.1));
+        errorLimitSpinner.setEditable(true);
+        
+        epochLimitSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100000, 5000, 100));
+        epochLimitSpinner.setEditable(true);
+    }
+    
+    private void setupButtons() {
+        trainingFilePathButton.setOnAction((actionEvent) -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+            fileChooser.setTitle("Wybierz plik treningowy");
+            fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Pliki tekstowe", "*.txt"));
+            
+            File selectedFile = fileChooser.showOpenDialog(null);
+            if (selectedFile != null) {
+               trainingFilePathField.setText(selectedFile.getAbsolutePath());
+            }
+        });
+        
+        testingFilePathButton.setOnAction((actionEvent) -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+            fileChooser.setTitle("Wybierz plik testowy");
+            fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Pliki tekstowe", "*.txt"));
+            
+            File selectedFile = fileChooser.showOpenDialog(null);
+            if (selectedFile != null) {
+               testingFilePathField.setText(selectedFile.getAbsolutePath());
+            }
+        });
+        
+        startLearningButton.setOnAction((actionEvent) -> {
+            SettingsFromUserTransportObject settings = getCurrentSettings();
+            
+            if (settings.checkIfCorrect()) {
+                MainApp.startLearning(settings);
+            } else {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Błąd");
+                alert.setHeaderText("Niepoprawne dane");
+                alert.setContentText("Przed rozpoczęciem nauki należy wprowadzić poprawne dane");
+                alert.showAndWait();
+            }
+        });
+    }
+    
+    private SettingsFromUserTransportObject getCurrentSettings() {
+        SettingsFromUserTransportObject settings = new SettingsFromUserTransportObject();
+        settings.epochLimit = epochLimitSpinner.getValue();
+        settings.errorLimit = errorLimitSpinner.getValue();
+        settings.inertialInfluence = inertiaSpinner.getValue();
+        settings.learningRate = learningRateSpinner.getValue();
+        settings.neuronCount = neuronCountSpinner.getValue();
+        settings.testingFile = testingFilePathField.getText();
+        settings.trainingFile = trainingFilePathField.getText();
+        settings.type = settingsTypeCombobox.getValue();
+        
+        return settings;
     }
 }
